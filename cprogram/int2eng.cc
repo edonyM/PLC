03fa4aecfa48e786f870225fd777601c2b2748d8
edonyM <edonyzpc@gmail.com>
2015-06-13 16:45:47 +0800

    fix the 11~19 bug

--- cprogram/int2eng.cc
+++ cprogram/int2eng.cc
@@ -39,11 +39,15 @@ string num2char[10] = {
                     "seven", "eight", "nine",
 };
 
-string num2ten[10] = {
+string num2ten[20] = {
                     "zero",
                     "ten", "twenty", "thirty",
                     "fourty", "fifty", "sixty",
                     "seventy", "eighty", "ninety",
+                    "ten",
+                    "eleven", "twelve", "thirteen",
+                    "fourteen", "fifteen", "sixteen",
+                    "seventeen", "eighteen", "nineteen"
 };
 
 string engcount[4] = {
@@ -89,12 +93,22 @@ string Num2Eng(string num) {
             tmp = tmp + t_tmp  + " hundreds ";
         }
         t_tmp = num2ten[int(num[1]-'0')];
+        if (t_tmp == "ten") {
+            int count = 10 + int(num[2]-'0');
+            tmp = tmp + num2ten[count] + " ";
+            return tmp;
+        }
         if (t_tmp != "zero")tmp = tmp + t_tmp + " ";
         t_tmp = num2char[int(num[2]-'0')];
         if (t_tmp != "zero")tmp = tmp + t_tmp + " ";
     }
     if (num.length() == 2) {
         string t_tmp = num2ten[int(num[0]-'0')];
+        if (t_tmp == "ten") {
+            int count = 10 + int(num[1]-'0');
+            tmp = tmp + num2ten[count] + " ";
+            return tmp;
+        }
         if (t_tmp != "zero") {
             tmp = tmp + t_tmp + " ";
         }
@@ -111,15 +125,21 @@ string Num2Eng(string num) {
 
 int main() {
     cout << "INT MAX" << INT_MAX <<endl;
-    int num;
+    int num, flag = 0;    // flag for record the negative
     cin >> num;
+    if (num < 0) {
+        flag = 1;
+        num = -num;
+    }
     string num_str = int2str(num);
     cout << "length: " << num_str<<endl;
     vector<string> vec_str;
     vec_str = SliceNum(num_str);
     cout << vec_str.size()<<endl;
     string results;
+    if (flag == 1) results += "minus ";
     for (size_t i=0; i< vec_str.size(); ++i) {
+        cout<<vec_str[i]<<endl;
         results = results + Num2Eng(vec_str[i]) + engcount[vec_str.size()-1-i] + " ";
     }
     cout <<results <<endl;
