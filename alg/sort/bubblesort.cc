ad7ddfea49c7aef1482758dd34b84c7148a2f6a9
edonyM <edonyzpc@gmail.com>
2015-07-23 15:17:11 +0800

    add sorting alg into sort direction

--- alg/sort/bubblesort.cc
+++ alg/sort/bubblesort.cc
@@ -15,9 +15,9 @@
  #
  # twitter : @edonyzpc
  #
- # Last modified: 2015-07-12 19:38
+ # Last modified: 2015-07-12 19:29
  #
- # Filename: insertsort.cc
+ # Filename: bubblesort.cc
  #
  # Description: All Rights Are Reserved
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
@@ -27,28 +27,40 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void coutls(int list[], int len) {
-    for (int i=0; i <len; ++i) cout << list[i] << " ";
-    cout << "*" << endl;
+void Swap(int *a, int *b) {
+    //*b ^= *a ^= *b ^= *a;
+    int tmp = 0;
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
-void InsertSort(int list[], int len) {
-    for (int i=1; i < len; ++i) {
-        int tmp = list[i];
-        int j;
-        for (j=i; j > 0; --j) {
-            if (tmp < list[j-1]) {
-                list[j] = list[j-1];
-            }
-            else {
-                break;
-            }
+
+void BubbleSort(int list[], int len) {
+    for (int j=len-1; j >= 0; --j){
+        int flag = 0;
+        for (int i=0; i <= j-1; ++i) {
+            if (list[i] > list[i + 1]) Swap(list+i, list+i+1);
+            else flag++;
         }
-        list[j] = tmp;
+        if (flag+1 == len) break;
     }
 }
 
 int main() {
-    int a[5] = { 12, 23, -1, 45, 0};
-    InsertSort(a, 5);
+    /* test Swap
+    int *a = new int;
+    int *b = new int;
+    *a = 12;
+    *b = 222;
+    Swap(a,b);
+    cout << *a << " " << *b << endl;
+    */
+    int a[5] = {2, 13, 4, 55,-1};
+    BubbleSort(a, 5);
     for (int i=0; i < 5; ++i) cout << a[i] << " ";
+    cout << endl;
+    int a1[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    BubbleSort(a1, 8);
+    for (int i=0; i < 8; ++i) cout << a1[i] << " ";
+    cout << endl;
 }
--- alg/sort/insertsort.cc
+++ alg/sort/insertsort.cc
@@ -15,9 +15,9 @@
  #
  # twitter : @edonyzpc
  #
- # Last modified: 2015-07-12 19:29
+ # Last modified: 2015-07-12 19:38
  #
- # Filename: bubblesort.cc
+ # Filename: insertsort.cc
  #
  # Description: All Rights Are Reserved
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
@@ -27,42 +27,33 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void Swap(int *a, int *b) {
-    //*b ^= *a ^= *b ^= *a;
-    int tmp = 0;
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+void coutls(int list[], int len) {
+    for (int i=0; i <len; ++i) cout << list[i] << " ";
+    cout << "*" << endl;
 }
-
-void BubbleSort(int list[], int len) {
-    for (int i=0; i < len; ++i) cout << list[i] << " ";
-    cout << endl;
-    for (int j=len; j >= 0; --j){
-        int flag = 0;
-        for (int i=0; i < j; ++i) {
-            if (list[i] > list[i + 1]) Swap(list+i, list+i+1);
-            else flag++;
+void InsertSort(int list[], int len) {
+    for (int i=1; i < len; ++i) {
+        int tmp = list[i];
+        int j;
+        for (j=i; j > 0; --j) {
+            if (tmp < list[j-1]) {
+                list[j] = list[j-1];
+            }
+            else {
+                break;
+            }
         }
-        if (flag == len) {cout<<flag<<endl;break;}
+        list[j] = tmp;
     }
 }
 
 int main() {
-    /* test Swap
-    int *a = new int;
-    int *b = new int;
-    *a = 12;
-    *b = 222;
-    Swap(a,b);
-    cout << *a << " " << *b << endl;
-    */
-    int a[5] = {2, 13, 4, 55,-1};
-    BubbleSort(a, 5);
+    int a[5] = { 12, 23, -1, 45, 0};
+    InsertSort(a, 5);
     for (int i=0; i < 5; ++i) cout << a[i] << " ";
     cout << endl;
     int a1[8] = {1, 2, 3, 4, 5, 6, 7, 8};
-    BubbleSort(a1, 8);
+    InsertSort(a1, 8);
     for (int i=0; i < 8; ++i) cout << a1[i] << " ";
     cout << endl;
 }
