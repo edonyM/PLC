486a954ce577bc81cd6026c6b974631d40bccf9b
edonyM <edonyzpc@gmail.com>
2015-09-27 18:55:01 +0800

    add string class

---
+++ jobs/str/class_string.cc
@@ -0,0 +1,40 @@
+ /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ #        .---.         .-----------
+ #       /     \  __  /    ------
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    /
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /)
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'
+ # ######################################################################################
+ #
+ # Author: edony - edonyzpc@gmail.com
+ #
+ # twitter : @edonyzpc
+ #
+ # Last modified: 2015-09-27 18:49
+ #
+ # Filename: class_string.cc
+ #
+ # Description: All Rights Are Reserved
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+// customized class string that focus on copy constructor, assignment constructor, default constructor and destructor.
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+class string {
+public:
+    string();
+    string(const string &other);
+    string(const char * str);
+    ~string();
+    string operator=(string &other);
+private:
+    char *data;
+};
